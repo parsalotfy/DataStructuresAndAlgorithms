f16f88bf853018e11d7352bde9b1c71ffb4fb2d5
Parsa Lotfi <parsalotfy@outlook.com>
2018-11-30 13:32:54 +0330

    Add constructors to Edge class

--- DataStructures/Edge.cs
+++ DataStructures/Edge.cs
@@ -2,6 +2,23 @@ namespace DataStructures
 {
     public class Edge<T> : IEdge<T>
     {
+        public Edge()
+        {
+
+        }
+
+        public Edge(INode<T> a_Node , INode<T> another_Node)
+        {
+            A_Node=a_Node;
+            Another_Node=another_Node;
+        }
+
+        public Edge(T a_Node , T another_Node)
+        {
+            A_Node = new Node<T>(a_Node);
+            Another_Node = new Node<T>(another_Node);
+        }
+
         public INode<T> A_Node { get; set; }
         public INode<T> Another_Node { get; set; }
     }
