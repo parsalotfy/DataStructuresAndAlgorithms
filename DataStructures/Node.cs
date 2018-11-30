22396c2bb4fcdf5bc0130881055b710b1ccce3da
Parsa Lotfi <parsalotfy@outlook.com>
2018-11-30 03:43:39 +0330

    Add constructor to Node class

--- DataStructures/Node.cs
+++ DataStructures/Node.cs
@@ -2,6 +2,16 @@ namespace DataStructures
 {
     public class Node<T> : INode<T>
     {
+        public Node()
+        {
+            
+        }
+
+        public Node(T nodeValue)
+        {
+            Value=nodeValue;
+        }
+
         public T Value { get; set; }
     }
 }
