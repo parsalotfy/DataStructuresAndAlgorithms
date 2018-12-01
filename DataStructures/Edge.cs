b4cee817af793cb124a36df90183b15e4e669486
Parsa Lotfi <parsalotfy@outlook.com>
2018-12-01 14:58:02 +0330

    Add non-generic Edge

--- DataStructures/Edge.cs
+++ DataStructures/Edge.cs
@@ -1,5 +1,30 @@
 namespace DataStructures
 {
+
+    public class Edge : IEdge
+    {
+
+        public Edge()
+        {
+
+        }
+
+        public Edge(INode a_Node, INode another_Node)
+        {
+            A_Node = a_Node;
+            Another_Node = another_Node;
+        }
+
+        public Edge(object a_Node, object another_Node)
+        {
+            A_Node = new Node(a_Node);
+            Another_Node = new Node(another_Node);
+        }
+
+        public INode A_Node { get; set; }
+        public INode Another_Node { get; set; }
+    }
+
     public class Edge<T> : IEdge<T>
     {
         public Edge()
@@ -21,5 +46,8 @@ namespace DataStructures
 
         public INode<T> A_Node { get; set; }
         public INode<T> Another_Node { get; set; }
+
+        INode IEdge.A_Node { get { return A_Node; } set { A_Node = (INode<T>)value; } }
+        INode IEdge.Another_Node { get => Another_Node; set => throw new System.NotImplementedException(); }
     }
 }
