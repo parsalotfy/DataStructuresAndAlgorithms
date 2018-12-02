fcda69f9748bbed24ef91f0afcb447ff7c54585f
Parsa Lotfi <parsalotfy@outlook.com>
2018-12-03 00:25:21 +0330

    1. Make setters private 2. Add lots of constructors 3. Add virtual UpdateValue methods

--- DataStructures/Edge.cs
+++ DataStructures/Edge.cs
@@ -4,73 +4,105 @@ namespace DataStructures
     public class Edge : IEdge
     {
 
+        #region  Constructors
+
         public Edge()
         {
 
         }
 
-        public Edge(INode a_Node, INode another_Node)
-        {
-            A_Node = a_Node;
-            Another_Node = another_Node;
-        }
-
         public Edge(object a_Node, object another_Node)
         {
-            A_Node = new Node(a_Node);
-            Another_Node = new Node(another_Node);
+            UpdateValue(a_Node, another_Node);
         }
 
-        public INode A_Node { get; set; }
-        public INode Another_Node { get; set; }
+        public Edge(INode a_Node, INode another_Node)
+        {
+            UpdateValue(a_Node, another_Node);
+        }
+
+        #endregion Constructors
+
+
+        #region  Properties
+
+        public INode A_Node { get; private set; }
+
+        public INode Another_Node { get; private set; }
+
+        #endregion  Properties
+
+
+        #region  Methods
+
+        public virtual void UpdateValue(object a_NodeValue, object another_NodeValue)
+        {
+            A_Node = new Node(a_NodeValue);
+            Another_Node = new Node(another_NodeValue);
+        }
+
+        public virtual void UpdateValue(INode a_NodeValue, INode another_NodeValue)
+        {
+            A_Node = a_NodeValue;
+            Another_Node = another_NodeValue;
+        }
+
+        #endregion  Methods
+
+
     }
+
+
+
+
 
     public class Edge<T> : Edge, IEdge<T>
     {
+        #region Constructors
+
+        // No object and INode Constructor
+
         public Edge() : base()
         {
 
         }
 
-        public Edge(INode<T> a_Node, INode<T> another_Node) : base(a_Node, another_Node)
-        {
-            A_Node = a_Node;
-            Another_Node = another_Node;
-        }
-
         public Edge(T a_Node, T another_Node) : base(a_Node, another_Node)
         {
-            A_Node = new Node<T>(a_Node);
-            Another_Node = new Node<T>(another_Node);
+            UpdateValue(a_Node, another_Node);
         }
 
-        private INode<T> _a_Node;
-        public new INode<T> A_Node
+        public Edge(INode<T> a_Node, INode<T> another_Node) : base(a_Node, another_Node)
         {
-            get
-            {
-                return _a_Node;
-            }
-            set
-            {
-                _a_Node = value;
-                base.A_Node = value;
-            }
+            UpdateValue(a_Node, another_Node);
         }
 
+        #endregion Constructors
 
-        private INode<T> _another_Node;
-        public new INode<T> Another_Node
+
+        #region Properties
+
+        public new INode<T> A_Node { get; private set; }
+
+        public new INode<T> Another_Node { get; private set; }
+
+        #endregion Properties
+
+
+        #region  Methods
+
+        public virtual void UpdateValue(T a_NodeValue, T another_NodeValue)
         {
-            get
-            {
-                return _another_Node;
-            }
-            set
-            {
-                _another_Node = value;
-                base.Another_Node=value;
-            }
+            A_Node = new Node<T>(a_NodeValue);
+            Another_Node = new Node<T>(another_NodeValue);
         }
+
+        public virtual void UpdateValue(INode<T> a_NodeValue, INode<T> another_NodeValue)
+        {
+            A_Node = a_NodeValue;
+            Another_Node = another_NodeValue;
+        }
+
+        #endregion  Methods
     }
 }
