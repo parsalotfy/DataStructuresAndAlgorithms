58dfa2ef475ed8e97b2331638de9f9e80360a5cd
Parsa Lotfi <parsalotfy@outlook.com>
2018-12-03 03:08:55 +0330

    Add other overloads of Update method into IEdge<T>

--- DataStructures/IEdgeT.cs
+++ DataStructures/IEdgeT.cs
@@ -5,6 +5,13 @@ namespace DataStructures
         new INode<T> A_Node { get; }
         new INode<T> Another_Node { get; }
 
-        void UpdateValue(INode<T> a_NodeValue, INode<T> another_NodeValue);
+
+        void UpdateEdge(T a_NodeValue, T another_NodeValue);
+        void Update_A_Node(T a_NodeValue);
+        void Update_Another_Node(T another_NodeValue);
+
+        void UpdateEdge(INode<T> a_NodeValue, INode<T> another_NodeValue);
+        void Update_A_Node(INode<T> a_NodeValue);
+        void Update_Another_Node(INode<T> another_NodeValue);
     }
 }
