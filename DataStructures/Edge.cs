b75fb51ecd09c1d9ea441d0970c916d082232f1d
Parsa Lotfi <parsalotfy@outlook.com>
2018-12-03 03:02:57 +0330

    Fix constructors and add other overloads of Updates methods.

--- DataStructures/Edge.cs
+++ DataStructures/Edge.cs
@@ -13,12 +13,12 @@ namespace DataStructures
 
         public Edge(object a_Node, object another_Node)
         {
-            UpdateValue(a_Node, another_Node);
+            UpdateEdge(a_Node, another_Node);
         }
 
         public Edge(INode a_Node, INode another_Node)
         {
-            UpdateValue(a_Node, another_Node);
+            UpdateEdge(a_Node, another_Node);
         }
 
         #endregion Constructors
@@ -35,16 +35,39 @@ namespace DataStructures
 
         #region  Methods
 
-        public virtual void UpdateValue(object a_NodeValue, object another_NodeValue)
+        public virtual void Update_A_Node(object a_NodeValue)
         {
             A_Node = new Node(a_NodeValue);
+        }
+
+        public virtual void Update_Another_Node(object another_NodeValue)
+        {
             Another_Node = new Node(another_NodeValue);
         }
 
-        public virtual void UpdateValue(INode a_NodeValue, INode another_NodeValue)
+        public virtual void UpdateEdge(object a_NodeValue, object another_NodeValue)
+        {
+            Update_A_Node(a_NodeValue);
+            Update_Another_Node(another_NodeValue);
+        }
+
+
+
+
+        public virtual void Update_A_Node(INode a_NodeValue)
         {
             A_Node = a_NodeValue;
+        }
+
+        public virtual void Update_Another_Node(INode another_NodeValue)
+        {
             Another_Node = another_NodeValue;
+        }
+
+        public virtual void UpdateEdge(INode a_NodeValue, INode another_NodeValue)
+        {
+            Update_A_Node(a_NodeValue);
+            Update_Another_Node(another_NodeValue);
         }
 
         #endregion  Methods
