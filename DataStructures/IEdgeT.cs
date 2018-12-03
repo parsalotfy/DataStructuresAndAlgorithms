4ec968cd3e026a58f1f9cc2f2c57aaa981cea079
Parsa Lotfi <parsalotfy@outlook.com>
2018-12-04 01:24:12 +0330

    Sign object updates to remove later

--- DataStructures/IEdgeT.cs
+++ DataStructures/IEdgeT.cs
@@ -6,9 +6,12 @@ namespace DataStructures
         new INode<T> Another_Node { get; }
 
 
+        #region Removable
         void UpdateEdge(T a_NodeValue, T another_NodeValue);
         void Update_A_Node(T a_NodeValue);
         void Update_Another_Node(T another_NodeValue);
+        #endregion Removable
+
 
         void UpdateEdge(INode<T> a_NodeValue, INode<T> another_NodeValue);
         void Update_A_Node(INode<T> a_NodeValue);
