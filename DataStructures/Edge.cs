e523a67b2804c773b381bcfd82903a39fa6964a2
Parsa Lotfi <parsalotfy@outlook.com>
2018-12-04 07:53:59 +0330

    Fix Edge for GetterSetterUpdate

--- DataStructures/Edge.cs
+++ DataStructures/Edge.cs
@@ -26,45 +26,61 @@ namespace DataStructures
 
         #region  Properties
 
-        public INode A_Node { get; private set; }
 
-        public INode Another_Node { get; private set; }
+        private INode _a_Node;
+        public INode A_Node
+        {
+            get { return _a_Node; }
+            set { Update_A_Node(value); }
+        }
+
+
+        private INode _another_Node;
+        public INode Another_Node
+        {
+            get { return _another_Node; }
+            set { Update_Another_Node(value); }
+        }
+
+
 
         #endregion  Properties
 
 
         #region  Methods
 
-        public virtual void Update_A_Node(object a_NodeValue)
+        public void Update_A_Node(object a_NodeValue)
         {
-            A_Node = new Node(a_NodeValue);
+            _a_Node = new Node(a_NodeValue);
         }
 
-        public virtual void Update_Another_Node(object another_NodeValue)
+        public void Update_Another_Node(object another_NodeValue)
         {
-            Another_Node = new Node(another_NodeValue);
+            _another_Node = new Node(another_NodeValue);
         }
 
-        public virtual void UpdateEdge(object a_NodeValue, object another_NodeValue)
+        public void UpdateEdge(object a_NodeValue, object another_NodeValue)
         {
             Update_A_Node(a_NodeValue);
             Update_Another_Node(another_NodeValue);
         }
 
 
-
-
-        public virtual void Update_A_Node(INode a_NodeValue)
+        // Just should be used in Setter
+        private void Update_A_Node(INode a_NodeValue)
         {
-            A_Node = a_NodeValue;
+            _a_Node = a_NodeValue;
         }
 
-        public virtual void Update_Another_Node(INode another_NodeValue)
+
+
+        // Just should be used in Setter
+        private void Update_Another_Node(INode another_NodeValue)
         {
-            Another_Node = another_NodeValue;
+            _another_Node = another_NodeValue;
         }
 
-        public virtual void UpdateEdge(INode a_NodeValue, INode another_NodeValue)
+        public void UpdateEdge(INode a_NodeValue, INode another_NodeValue)
         {
             Update_A_Node(a_NodeValue);
             Update_Another_Node(another_NodeValue);
