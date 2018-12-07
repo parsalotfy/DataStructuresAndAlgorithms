6c4763217f9f9bbe790a20f58d4c9c9888e91872
Parsa Lotfi <parsalotfy@outlook.com>
2018-12-08 03:16:18 +0330

    clear code

--- DataStructures/IGraphT.cs
+++ DataStructures/IGraphT.cs
@@ -2,16 +2,9 @@ using System.Collections.Generic;
 
 namespace DataStructures
 {
-// Ctors
-// Ctors chaining
-// IEquality
-// Update method for Node and Edge
-// add overloads of Update(object) to IEdge and IEdgeT
-//
-
     public interface IGraph<T> : IGraph
     {
-        new ISet<INode<T>> Nodes { get; set; }
-        new ISet<IEdge<T>> Edges { get; set; }
+        new ISet<INode<T>> Nodes { get; }
+        new ISet<IEdge<T>> Edges { get; }
     }
 }
