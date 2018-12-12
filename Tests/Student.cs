6c7c8002f0041cb913640ed75c49d077f5ac81cf
Parsa Lotfi <parsalotfy@outlook.com>
2018-12-13 02:15:38 +0330

    Add student class

---
+++ Tests/Student.cs
@@ -0,0 +1,11 @@
+namespace Tests
+{
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+}
