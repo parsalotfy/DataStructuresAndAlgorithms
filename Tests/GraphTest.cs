848183fb2c25fcdf3dcec6e7f63287236a97172d
Parsa Lotfi <parsalotfy@outlook.com>
2018-12-13 02:23:50 +0330

    Clear graph tests

--- Tests/GraphTest.cs
+++ Tests/GraphTest.cs
@@ -11,44 +11,6 @@ namespace Tests
         [Fact]
         public void EdgeT_Value_Test()
         {
-            Graph g = new Graph(INodeEqualityComparer.Instance, IEdgeEqualityComparer.Instance);
-
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(1);
-
-
-            HashSet<INode> nodes = new HashSet<INode>(INodeEqualityComparer.Instance);
-            nodes.Add(n1);
-            nodes.Add(n2);
-            nodes.Add(n3);
-            nodes.Add(n4);
-
-            Node<int> a = new Node<int>(1);
-            var b = (Node)a;
-            
-            // IEqualityComparer<INode> intcomparer = sample;
-            // IEqualityComparer<INode<int>> sample = EqualityComparer<INode<int>>.Default;
-            
-
-
-
-
-            g.AddNode(n1);
-            g.AddNode(n2);
-            g.AddNode(n3);
-            g.AddNode(n4);
-
-            // g.AddEdge(new Edge(n1, n6));
-            // g.AddEdge(new Edge(n2, n6));
-            // g.AddEdge(new Edge(n3, n6));
-            // g.AddEdge(new Edge(n4, n6));
-            // g.AddEdge(new Edge(n5, n6));
-            // g.AddEdge(new Edge(n7, n6));
-
-            Assert.True(true);
-
 
         }
     }
