21bc3cf6d1ef001d4259ee4c10bce294362e22f6
parsalotfy <parsalotfy@outlook.com>
2018-12-14 15:31:29 +0330

    Create reusable EqualityComparer for Nodes and Edges and etc.

---
+++ DataStructures/TEqualityComparer.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using DataStructures;
+
+namespace DataStructures
+{
+    public class TEqualityComparer<T> : EqualityComparer<T>
+    {
+        private TEqualityComparer()
+        {
+
+        }
+
+        public TEqualityComparer(Func<T,T,bool> equalsMethod, Func<T, int> getHashCodeMethod)
+        {
+            EqualsMethod=equalsMethod;
+            GetHashCodeMethod=getHashCodeMethod;
+        }
+
+        public Func<T,T,bool> EqualsMethod { get; }
+        public Func<T, int> GetHashCodeMethod { get; }
+
+
+        public override bool Equals(T x, T y)
+        {
+            return EqualsMethod(x,y);
+        }
+
+        public override int GetHashCode(T t)
+        {
+            return GetHashCodeMethod(t);
+        }
+    }
+}
