7c562b65092c9fa79f8b01381fbd8aade816cb1f
parsalotfy <parsalotfy@outlook.com>
2019-10-08 16:42:07 +0330

    Fix and remove parts including INode and Node.

--- DataStructures/EdgeT.cs
+++ DataStructures/EdgeT.cs
@@ -4,14 +4,7 @@ namespace DataStructures
     {
         #region Constructors
 
-        // No object and INode Constructor
         public Edge(T a_Node, T another_Node)
-        {
-            _a_Node = new Node<T>(a_Node);
-            _another_Node = new Node<T>(another_Node);
-        }
-
-        public Edge(INode<T> a_Node, INode<T> another_Node)
         {
             _a_Node = a_Node;
             _another_Node = another_Node;
@@ -22,15 +15,15 @@ namespace DataStructures
 
         #region Properties
 
-        private readonly INode<T> _a_Node;
-        public INode<T> A_Node
+        private readonly T _a_Node;
+        public T A_Node
         {
             get { return _a_Node; }
         }
 
 
-        private readonly INode<T> _another_Node;
-        public INode<T> Another_Node
+        private readonly T _another_Node;
+        public T Another_Node
         {
             get { return _another_Node; }
         }
--- DataStructures/IEdgeT.cs
+++ DataStructures/IEdgeT.cs
@@ -2,7 +2,7 @@ namespace DataStructures
 {
     public interface IEdge<T>
     {
-        INode<T> A_Node { get; }
-        INode<T> Another_Node { get; }
+        T A_Node { get; }
+        T Another_Node { get; }
     }
 }
