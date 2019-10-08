3230863f15c350786e703e36bf4898e8ec9fbce9
parsalotfy <parsalotfy@outlook.com>
2019-10-08 23:48:34 +0330

    Fix test for Edge.

--- Tests/EdgeTTest.cs
+++ Tests/EdgeTTest.cs
@@ -3,36 +3,19 @@ using Xunit;
 
 namespace Tests
 {
-    public class EdgeTTest
+    public class EdgeTest
     {
         [Fact]
-        public void EdgeT_Constructor_T_Test()
+        public void Edge_Constructor_T_Test()
         {
             // Arrange
-            Edge<int> e = new Edge<int>(2, 3);
+            Edge<int> e = new Edge<int>(2,3);
 
             // Act
 
             // Assert
-            Assert.Equal(2, e.A_Node.Value);
-            Assert.Equal(3, e.Another_Node.Value);
-        }
-
-
-        [Fact]
-        public void EdgeT_Constructor_INodeT_Test()
-        {
-            // Arrange
-            INode<int> n1 = new Node<int>(2);
-            INode<int> n2 = new Node<int>(3);
-
-            Edge<int> e = new Edge<int>(n1, n2);
-
-            // Act
-
-            // Assert
-            Assert.Equal(2, e.A_Node.Value);
-            Assert.Equal(3, e.Another_Node.Value);
-        }
+            Assert.Equal(2, e.A_Node);
+            Assert.Equal(3, e.Another_Node);
+        }        
     }
 }
