3f27ca8580792e07829775bd5acafd90ace63b3f
parsalotfy <parsalotfy@outlook.com>
2019-10-14 00:43:44 +0330

    Add GraphADT( Student ) test method.

--- Tests/GraphTest.cs
+++ Tests/GraphTest.cs
@@ -9,7 +9,7 @@ namespace Tests
     public class GraphTest
     {
         [Fact]
-        public void Graph_P_Null_AddNode_AddEdge()
+        public void Graph_P_Null_AddNode_AddEdge_RemoveNode_RemoveEdge()
         {
             // Arrange
             Graph<int> intGraph = new Graph<int>();
@@ -39,6 +39,47 @@ namespace Tests
             intGraph.RemoveEdge(1, 1);
             intGraph.RemoveEdge(2, 3);
 
+
+
+            // Assert
+            Assert.Equal(5, intGraph.Nodes.Count());
+            Assert.Equal(4, intGraph.Edges.Count());
+        }
+
+
+        [Fact]
+        public void Graph_ADT_Null_AddNode_AddEdge_RemoveNode_RemoveEdge()
+        {
+            // Arrange
+            Graph<Student> intGraph = new Graph<Student>((n1, n2) => n1.ID == n2.ID, n => n.ID.GetHashCode());
+
+            // Act
+            intGraph.AddNode(new Student(1, "a", 15));
+            intGraph.AddNode(new Student(2, "b", 16));
+            intGraph.AddNode(new Student(3, "c", 17));
+            intGraph.AddNode(new Student(4, "d", 18));
+            intGraph.AddNode(new Student(5, "e", 19));
+            intGraph.AddNode(new Student(6, "f", 20));
+
+            intGraph.AddEdge(new Student(1, "a", 15), new Student(6, "f", 20));
+            intGraph.AddEdge(new Student(2, "b", 16), new Student(6, "f", 20));
+            intGraph.AddEdge(new Student(3, "c", 17), new Student(6, "f", 20));
+            intGraph.AddEdge(new Student(4, "d", 18), new Student(6, "f", 20));
+            intGraph.AddEdge(new Student(5, "e", 19), new Student(6, "f", 20));
+
+            intGraph.AddEdge(new Student(1, "a", 15), new Student(1, "a", 15));
+            intGraph.AddEdge(new Student(2, "b", 16), new Student(2, "b", 16));
+            intGraph.AddEdge(new Student(3, "c", 17), new Student(3, "c", 17));
+            intGraph.AddEdge(new Student(4, "d", 18), new Student(4, "d", 18));
+            intGraph.AddEdge(new Student(5, "e", 19), new Student(5, "e", 19));
+
+            intGraph.AddEdge(new Student(2, "b", 16), new Student(3, "c", 17));
+
+
+            intGraph.RemoveNode(new Student(6, "f", 20));
+            intGraph.RemoveEdge(new Student(1, "a", 15), new Student(1, "a", 15));
+            //intGraph.RemoveEdge(new Student(3, "c", 17), new Student(2, "b", 16));
+            intGraph.RemoveEdge(new Student(2, "b", 16), new Student(3, "c", 17));
 
 
             // Assert
