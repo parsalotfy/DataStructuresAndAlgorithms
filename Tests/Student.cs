716537a627d41e8a675113bef94bde90717fc401
parsalotfy <parsalotfy@outlook.com>
2019-10-14 00:42:25 +0330

    Add constructor to Student class.

--- Tests/Student.cs
+++ Tests/Student.cs
@@ -3,6 +3,14 @@ namespace Tests
 
     public class Student
     {
+
+        public Student(int id, string name, int age)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+        }
+
         public int ID { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
