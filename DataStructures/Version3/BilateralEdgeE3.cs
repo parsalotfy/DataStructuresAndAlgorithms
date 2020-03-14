6b43a1661e9e4d06e0a5f5bf1c36957b916c94d9
parsalotfy <parsalotfy@outlook.com>
2020-03-14 19:02:21 +0330

    Add Version 3

---
+++ DataStructures/Version3/BilateralEdgeE3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataStructures.Version3
+{
+    public class BilateralEdgeE3<T> : UnilateralEdgeE3<T>, IEquatable<BilateralEdgeE3<T>> where T : IEquatable<T>
+    {
+        public BilateralEdgeE3(T source, T target) : base(source, target)
+        {
+        }
+
+        public bool Equals(BilateralEdgeE3<T> other)
+        {
+            return base.Equals(other) || (Source.Equals(other.Target) && Target.Equals(other.Source));
+        }
+
+        public override int GetHashCode()
+        {
+            return Source.GetHashCode() ^ Target.GetHashCode();
+        }
+    }
+}
