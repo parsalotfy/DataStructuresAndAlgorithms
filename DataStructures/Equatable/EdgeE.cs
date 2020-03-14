42528db6faaae81683512648a3b2150d253cefe9
parsalotfy <parsalotfy@outlook.com>
2020-03-14 19:01:55 +0330

    Add sources linkes to another file

--- DataStructures/Equatable/EdgeE.cs
+++ DataStructures/Equatable/EdgeE.cs
@@ -2,14 +2,6 @@
 
 namespace DataStructures.Equatable
 {
-    // https://stackoverflow.com/questions/9316918/what-is-the-difference-between-iequalitycomparert-and-iequatablet
-    // https://stackoverflow.com/questions/8952003/how-does-hashset-compare-elements-for-equality/15413983
-    // https://stackoverflow.com/questions/58379115/iequatablet-with-gethashcode
-    // https://stackoverflow.com/questions/19791178/force-child-class-to-override-function-of-ancestor-via-parent
-    // https://docs.microsoft.com/en-us/dotnet/api/system.iequatable-1?view=netframework-4.8
-    // https://softwareengineering.stackexchange.com/questions/307999/can-i-enforce-the-overriding-of-gethashcode-and-equals-methods-for-users-of
-    // https://stackoverflow.com/questions/49055673/should-gethashcode-be-implemented-for-iequatablet-on-mutable-types
-
     public class EdgeE<T> : IEdgeE<T>, IEquatable<EdgeE<T>> where T : IEquatable<T>
     {
 
