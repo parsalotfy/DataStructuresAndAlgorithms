9a13b24ab948290921baf73e0718f9c10071b54f
parsalotfy <parsalotfy@outlook.com>
2020-03-17 17:52:31 +0330

    Implement Graph Equatable Explicit version 3

---
+++ DataStructures/Version3/GraphExplicit/IGraphEqEx3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Version3.GraphExplicit
+{
+    public interface IGraphEqEx3<T> where T : IEquatable<T>
+    {
+        ISet<T> Nodes { get; }
+        ISet<Unilateral_EdgeE3<T>> Edges { get; }
+    }
+}
---
+++ DataStructures/Version3/GraphExplicit/Unilateral_Labelled_GraphEqEx3.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures.Version3.GraphExplicit
+{
+    public class Unilateral_Labelled_GraphEqEx3<T> : IGraphEqEx3<T> where T : IEquatable<T>
+    {
+
+        #region Constructors
+
+        // Primitive Graph Null
+        public Unilateral_Labelled_GraphEqEx3()
+        {
+            _nodes = new HashSet<T>();
+            _edges = new HashSet<Unilateral_LabelledEdgeE3<T>>();
+        }
+
+        // Primitive Graph Node
+        public Unilateral_Labelled_GraphEqEx3(ISet<T> nodes) : this() 
+        {
+            _nodes.UnionWith(nodes);
+        }
+
+        // Primitive Graph Node+Edge
+        public Unilateral_Labelled_GraphEqEx3(ISet<T> nodes, ISet<Unilateral_LabelledEdgeE3<T>> edges) : this(nodes)
+        {
+            _edges.UnionWith(edges);
+        }
+
+        #endregion Constructors
+
+
+        #region Properties
+
+        private readonly HashSet<T> _nodes;
+        public ISet<T> Nodes
+        {
+            get { return _nodes; }
+        }
+
+        private readonly HashSet<Unilateral_LabelledEdgeE3<T>> _edges;
+        public ISet<Unilateral_LabelledEdgeE3<T>> Edges
+        {
+            get { return _edges; }
+        }
+
+        // ?!?!????!
+        ISet<Unilateral_EdgeE3<T>> IGraphEqEx3<T>.Edges => throw new NotImplementedException();
+
+
+
+        #endregion Properties
+
+
+        #region Methods
+
+        public bool AddNode(T node)
+        {
+            return _nodes.Add(node);
+        }
+
+        // Not sure about this method.
+        public void AddNode(params T[] nodes)
+        {
+            foreach (T node in nodes)
+            {
+                AddNode(node);
+            }
+        }
+
+        public bool AddEdge(Unilateral_LabelledEdgeE3<T> edge)
+        {
+            if (_edges.Add(edge) == true)
+            {
+                AddNode(edge.Source);
+                AddNode(edge.Target);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool AddEdge(T source, T target , string label)
+        {
+            return AddEdge(new Unilateral_LabelledEdgeE3<T>(source, target, label));
+        }
+
+        public bool RemoveNode(T node)
+        {
+            if (_nodes.RemoveWhere(n => n.Equals(node)) > 0)
+            {
+                _edges.RemoveWhere(e => e.Source.Equals(node) || e.Target.Equals(node));
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        // Not sure about this method.
+        public void RemoveNode(params T[] nodes)
+        {
+            foreach (T node in nodes)
+            {
+                RemoveNode(node);
+            }
+        }
+
+        public bool RemoveEdge(Unilateral_EdgeE3<T> edge)
+        {
+            return _edges.RemoveWhere(e => e.Equals(edge)) > 0;
+        }
+
+        public bool RemoveEdge(T aNode, T anotherNode)
+        {
+            return RemoveEdge(new Unilateral_EdgeE3<T>(aNode, anotherNode));
+        }
+
+        public void ClearGraph()
+        {
+            _edges.Clear();
+            _nodes.Clear();
+        }
+
+        #endregion Methods
+
+    }
+}
